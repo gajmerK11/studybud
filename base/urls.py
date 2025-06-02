236016d59b28073ee80fdcd91035089b55ed1640
gajmerkumar <gajmerk17@gmail.com>
2025-06-02 14:51:49 +0545

    Mon,Jun-2(day)

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    # to make the each value of list 'rooms' clickable and to pass the id to the url we have added '<str:pk>' i.e. we are passing a dynamic value here  
+    path('room/<str:pk>/', views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+# we have created this 'rooms' list of dictionaries to pass data
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
+
 def home(request):
     # when this function will be triggered, it will render 'home.html' file
     # render function takes 2 parameters: first one 'request' object, second one 'template' that we want to render
-    return render(request, 'home.html')
+    # we have created this 'context' variable just to store the data that we want to pass
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 # anoter route
 # our website is going to have rooms for different conversations so we are creating a route for it
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    # below code is for dynamically getting values of 'rooms' in respective url with id
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
